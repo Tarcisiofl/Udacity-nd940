68997b0eacc97a79607f4d15c3a9929f49a482bd
Tarcisiofl <tarcisiofl@si.ufu.br>
2021-05-13 15:53:57 -0300

    Exercise: Display SleepQuality Data

--- SleepTracker-RecyclerView/app/src/main/java/br/com/tarcisiofl/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ SleepTracker-RecyclerView/app/src/main/java/br/com/tarcisiofl/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -15,3 +15,31 @@
  */
 
 package br.com.tarcisiofl.trackmysleepquality.sleeptracker
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import br.com.tarcisiofl.trackmysleepquality.R
+import br.com.tarcisiofl.trackmysleepquality.TextItemViewHolder
+import br.com.tarcisiofl.trackmysleepquality.database.SleepNight
+
+class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
+    var data = listOf<SleepNight>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun getItemCount() = data.size
+    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+        val item = data[position]
+        holder.textView.text = item.sleepQuality.toString()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.text_item_view, parent, false) as TextView
+        return TextItemViewHolder(view)
+    }
+}
