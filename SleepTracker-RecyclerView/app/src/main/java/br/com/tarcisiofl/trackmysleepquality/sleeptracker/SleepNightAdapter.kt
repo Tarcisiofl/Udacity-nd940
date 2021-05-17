00de91b4492cfba0ba96b21a2f25e49d52ef2a9c
Tarcisiofl <tarcisiofl@si.ufu.br>
2021-05-17 11:18:30 -0300

    Exercise: Refresh Data with DiffUtil

--- SleepTracker-RecyclerView/app/src/main/java/br/com/tarcisiofl/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ SleepTracker-RecyclerView/app/src/main/java/br/com/tarcisiofl/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -21,26 +21,22 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import br.com.tarcisiofl.trackmysleepquality.R
 import br.com.tarcisiofl.trackmysleepquality.convertDurationToFormatted
 import br.com.tarcisiofl.trackmysleepquality.convertNumericQualityToString
 import br.com.tarcisiofl.trackmysleepquality.database.SleepNight
 
-class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
-    var data = listOf<SleepNight>()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
+class SleepNightAdapter : ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(SleepNightDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder.from(parent)
     }
 
-    override fun getItemCount() = data.size
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = data[position]
+        val item = getItem(position)
         holder.bind(item)
     }
 
@@ -72,4 +68,15 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
             }
         }
     }
+}
+
+class SleepNightDiffCallback : DiffUtil.ItemCallback<SleepNight>() {
+    override fun areItemsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+        return oldItem.nightId == newItem.nightId
+    }
+
+    override fun areContentsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+        return oldItem == newItem
+    }
+
 }
