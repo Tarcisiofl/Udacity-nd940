0037a1bd97aadb2ef62169be1b7d30737d9423b2
Tarcisiofl <tarcisiofl@si.ufu.br>
2021-05-17 11:47:31 -0300

    Exercise: Add Binding Adapters

--- SleepTracker-RecyclerView/app/src/main/java/br/com/tarcisiofl/trackmysleepquality/sleeptracker/BindingUtils.kt
+++ SleepTracker-RecyclerView/app/src/main/java/br/com/tarcisiofl/trackmysleepquality/sleeptracker/BindingUtils.kt
@@ -15,3 +15,40 @@
  */
 
 package br.com.tarcisiofl.trackmysleepquality.sleeptracker
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import br.com.tarcisiofl.trackmysleepquality.R
+import br.com.tarcisiofl.trackmysleepquality.convertDurationToFormatted
+import br.com.tarcisiofl.trackmysleepquality.convertNumericQualityToString
+import br.com.tarcisiofl.trackmysleepquality.database.SleepNight
+
+@BindingAdapter("sleepDurationFormatted")
+fun TextView.setSleepDurationFormatted(item: SleepNight?) {
+    item?.let {
+        text = convertDurationToFormatted(it.startTimeMilli, item.endTimeMilli, context.resources)
+    }
+}
+
+@BindingAdapter("sleepQualityString")
+fun TextView.setSleepQualityString(item: SleepNight?) {
+    item?.let {
+        text = convertNumericQualityToString(item.sleepQuality, context.resources)
+    }
+}
+
+@BindingAdapter("sleepImage")
+fun ImageView.setSleepImage(item: SleepNight?) {
+    item?.let {
+        setImageResource(when (item.sleepQuality) {
+            0 -> R.drawable.ic_sleep_0
+            1 -> R.drawable.ic_sleep_1
+            2 -> R.drawable.ic_sleep_2
+            3 -> R.drawable.ic_sleep_3
+            4 -> R.drawable.ic_sleep_4
+            5 -> R.drawable.ic_sleep_5
+            else -> R.drawable.ic_sleep_active
+        })
+    }
+}
