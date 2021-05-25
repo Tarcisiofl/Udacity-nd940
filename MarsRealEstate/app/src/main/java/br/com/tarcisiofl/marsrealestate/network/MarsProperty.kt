8dcb4a747641470e013c9d46bbb1e41ee133c6da
Tarcisiofl <tarcisiofl@si.ufu.br>
2021-05-25 10:39:26 -0300

    Exercise: Add the Detail Screen

--- MarsRealEstate/app/src/main/java/br/com/tarcisiofl/marsrealestate/network/MarsProperty.kt
+++ MarsRealEstate/app/src/main/java/br/com/tarcisiofl/marsrealestate/network/MarsProperty.kt
@@ -17,11 +17,18 @@
 
 package br.com.tarcisiofl.marsrealestate.network
 
+import android.os.Parcelable
 import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class MarsProperty(
         val id: String,
         @Json(name = "img_src") val imgSrcUrl: String,
         val type: String,
         val price: Double
-)
+) : Parcelable {
+    val isRental
+        get() = type == "rent"
+}
+
