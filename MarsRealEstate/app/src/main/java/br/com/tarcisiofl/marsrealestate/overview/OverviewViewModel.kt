be325d82cdc9df00b74980bd78929bab299c43f0
Tarcisiofl <tarcisiofl@si.ufu.br>
2021-05-25 05:32:49 -0300

    12. Exercise: Display an Internet Image

--- MarsRealEstate/app/src/main/java/br/com/tarcisiofl/marsrealestate/overview/OverviewViewModel.kt
+++ MarsRealEstate/app/src/main/java/br/com/tarcisiofl/marsrealestate/overview/OverviewViewModel.kt
@@ -22,6 +22,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import br.com.tarcisiofl.marsrealestate.network.MarsApi
+import br.com.tarcisiofl.marsrealestate.network.MarsProperty
 import kotlinx.coroutines.launch
 
 /**
@@ -30,11 +31,16 @@ import kotlinx.coroutines.launch
 class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData String that stores the status of the most recent request
-    private val _response = MutableLiveData<String>()
+    private val _status = MutableLiveData<String>()
 
     // The external immutable LiveData for the request status String
-    val response: LiveData<String>
-        get() = _response
+    val status: LiveData<String>
+        get() = _status
+
+    private val _property = MutableLiveData<MarsProperty>()
+
+    val property: LiveData<MarsProperty>
+        get() = _property
 
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
@@ -50,9 +56,11 @@ class OverviewViewModel : ViewModel() {
         viewModelScope.launch {
             try {
                 var listResult = MarsApi.retrofitService.getProperties()
-                _response.value = "Success: ${listResult.size} Mars properties retrieved"
+                _status.value = "Success: ${listResult.size} Mars properties retrieved"
+                if (listResult.isNotEmpty())
+                    _property.value = listResult[0]
             } catch (e: Exception) {
-                _response.value = "Failure: ${e.message}"
+                _status.value = "Failure: ${e.message}"
             }
         }
     }
